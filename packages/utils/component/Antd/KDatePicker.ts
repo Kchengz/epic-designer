041036d029e641a84922fbfd01200f549b799e54
kcz <1150791676@qq.com>
2023-01-10 16:24:38 +0800

    feat: 封装date-picker组件

---
+++ packages/utils/component/Antd/KDatePicker.ts
@@ -0,0 +1,48 @@
+import { defineComponent, h, watch } from "vue";
+import datePicker from "ant-design-vue/lib/date-picker";
+// 二次封装组件
+export default defineComponent({
+  emits: ["update:value"],
+  setup(_, { emit, attrs }) {
+    watch(
+      () => attrs.type,
+      () => {
+        handleUpdate();
+      }
+    );
+
+    function handleUpdate(e = null) {
+      emit(`update:value`, e);
+    }
+    return {
+      attrs,
+      handleUpdate,
+    };
+  },
+  render() {
+    let cmp: any = datePicker;
+    const type = this.attrs.type;
+
+    const props: { [propName: string]: any } = {
+      ...this.attrs,
+      "onUpdate:value": this.handleUpdate,
+    };
+
+    // 判断显示类型，渲染相应组件
+    if (type === "daterange") {
+      // 默认值与组件类型不匹配时需清空默认值
+      if (typeof props.value !== "object" && props.value !== null)
+        props.value = null;
+      cmp = datePicker.RangePicker;
+    } else if (type === "month") {
+      // 默认值与组件类型不匹配时需清空默认值
+      if (typeof props.value === "object") props.value = null;
+      cmp = datePicker.MonthPicker;
+    } else {
+      // 默认值与组件类型不匹配时需清空默认值
+      if (typeof props.value === "object") props.value = null;
+    }
+
+    return [h(cmp, props)];
+  },
+});
