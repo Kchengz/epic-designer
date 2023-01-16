b8c844e1bd4f60d68c170804d3b45305bd998c7b
kcz <1150791676@qq.com>
2023-01-17 00:19:00 +0800

    chore: 精简KCard组件代码

--- packages/utils/component/Antd/KCard.ts
+++ packages/utils/component/Antd/KCard.ts
@@ -1,13 +1,7 @@
 import { defineComponent, h, renderSlot } from "vue";
 import Card from "ant-design-vue/lib/card";
-import { NodeItem } from '../../../types/kDesigner'
-// 二次封装组件
 export default defineComponent({
-  emits: ["update:value"],
   setup(_, { attrs, slots }) {
-
-    console.log(attrs)
-
     return {
       attrs,
       slots
