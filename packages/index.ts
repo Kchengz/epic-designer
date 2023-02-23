1234eebe78c6530c07ac8b641bb5823481d89e37
kcz <1150791676@qq.com>
2023-02-23 22:37:33 +0800

    chore: 表单修改为单独组件，支持多表单

--- packages/index.ts
+++ packages/index.ts
@@ -10,59 +10,6 @@ initComponent(pluginManager);
 const components = [KBuilder, KDesigner];
 
 
-pluginManager.addComponent(
-  () => import("./components/KForm/index"),
-  {
-    label: "表单",
-    type: "form",
-    icon: "icon-qiapian",
-    labelWidth: 100,
-    children: [],
-  },
-  [
-    {
-      label: "标签布局",
-      type: "radio",
-      attrIndex: "componentProps.labelLayout",
-      componentProps: {
-        options: [
-          {
-            label: "固定宽度",
-            value: "fixed",
-          },
-          {
-            label: "自适应宽度",
-            value: "flex",
-          },
-        ],
-      },
-    },
-    {
-      label: "labelWidth",
-      type: "input",
-      attrIndex: "componentProps.labelWidth",
-      show: (values) => {
-        return values.componentProps.labelLayout === "fixed";
-      },
-    },
-    {
-      label: "labelCol",
-      type: "input",
-      attrIndex: "componentProps.labelCol.span",
-      show: (values) => {
-        return values.componentProps.labelLayout === "flex";
-      },
-    },
-    {
-      label: "wrapperCol",
-      type: "input",
-      attrIndex: "componentProps.wrapperCol.span",
-      show: (values) => {
-        return values.componentProps.labelLayout === "flex";
-      },
-    },
-  ]
-);
 // 注册全局组件
 const KDesignr = {
   install(app: App) {
