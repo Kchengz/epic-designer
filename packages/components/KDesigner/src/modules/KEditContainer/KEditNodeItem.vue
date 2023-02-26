bcbcda1d729e971b774ac081b0bddda6fa4a3b47
kcz <1150791676@qq.com>
2023-02-26 15:24:16 +0800

    chore: 移除KNodeItem属性监听，防止触发所有子组件重新渲染

--- packages/components/KDesigner/src/modules/KEditContainer/KEditNodeItem.vue
+++ packages/components/KDesigner/src/modules/KEditContainer/KEditNodeItem.vue
@@ -3,7 +3,7 @@
     @start="handleSelect($event.oldIndex); designer.setDisableHover(true)" @end="handleEnd()"
     @add="handleSelect($event.newIndex)" ghostClass="moveing" :component-data="{ name: 'draggable-range' }">
     <template #item="{ element, index }">
-      <div v-if="element.type !== 'col'" class="item" :class="{
+      <div v-if="!['col'].includes(element.type)" class="item" :class="{
         checked: designer.state.checkedNode?.id === element.id,
         hover: designer.state.hoverNode?.id === element.id,
         'root-node': element.id === 'root'
@@ -24,12 +24,7 @@
         </div>
         <KNodeItem :element="element" />
       </div>
-      <KNodeItem v-else class="item" :class="{
-        checked: designer.state.checkedNode?.id === element.id,
-        hover: designer.state.hoverNode?.id === element.id,
-        'root-node': element.id === 'root'
-      }" @click.stop="designer.setCheckedNode(element)" @mouseover.stop="designer.setHoverNode(element)"
-        @mouseout.stop="designer.setHoverNode(null)" :element="element" />
+      <KNodeItem v-else :element="element" />
     </template>
   </draggable>
 </template>
