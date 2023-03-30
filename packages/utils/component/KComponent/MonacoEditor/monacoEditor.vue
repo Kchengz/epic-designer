a894e9ce3cd7207656091004c1e8722c1693a50f
kchengz <1150791676@qq.com>
2023-03-31 00:51:42 +0800

    chore: 修改monacoEditor组件

--- packages/utils/component/KComponent/MonacoEditor/monacoEditor.vue
+++ packages/utils/component/KComponent/MonacoEditor/monacoEditor.vue
@@ -3,17 +3,19 @@
 </template>
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import * as monaco from 'monaco-editor'
-// import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js'
-// import 'monaco-editor/esm/vs/language/json/monaco.contribution'
+// import * as monaco from 'monaco-editor'
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js'
+import 'monaco-editor/esm/vs/editor/edcore.main.js'
+import 'monaco-editor/esm/vs/language/json/monaco.contribution'
+import 'monaco-editor/esm/vs/language/typescript/monaco.contribution'
 // @ts-ignore
-import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+// import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
 // @ts-ignore
 import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
 // @ts-ignore
-import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+// import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
 // @ts-ignore
-import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+// import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 // @ts-ignore
 import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
@@ -23,16 +25,16 @@ self.MonacoEnvironment = {
     if (label === 'json') {
       return new JsonWorker()
     }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new CssWorker()
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new HtmlWorker()
-    }
+    // if (label === 'css' || label === 'scss' || label === 'less') {
+    //   return new CssWorker()
+    // }
+    // if (label === 'html' || label === 'handlebars' || label === 'razor') {
+    //   return new HtmlWorker()
+    // }
     if (label === 'typescript' || label === 'javascript') {
       return new TsWorker()
     }
-    return new EditorWorker()
+    // return new EditorWorker()
   }
 }
 
@@ -42,7 +44,7 @@ const props = defineProps({
   },
   language: {
     type: String,
-    default: 'json'
+    default: 'javascript'
   }
 })
 
