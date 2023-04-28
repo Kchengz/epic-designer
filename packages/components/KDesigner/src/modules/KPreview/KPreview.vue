662c3d782046e2c886827324aa6526b72a1d1c53
kangcz <1150791676@qq.com>
2023-04-28 21:41:36 +0800

    chore: 预览组件每次预览重新加载组件

--- packages/components/KDesigner/src/modules/KPreview/KPreview.vue
+++ packages/components/KDesigner/src/modules/KPreview/KPreview.vue
@@ -1,6 +1,6 @@
 <template>
-  <component v-if="Modal" :is="Modal" title="预览" v-model:visible="visible" v-model="visible" width="800px" :footer="null">
-    <KBuilder ref="kb" :schemas="schemas" :script="script" />
+  <component :is="Modal" title="预览" v-model:visible="visible" v-model="visible" width="800px" :footer="null">
+    <KBuilder :key="kBuilderKey" ref="kb" :schemas="schemas" :script="script" />
     <div class="k-modal-footer">
       <Button @click="handleClose">关闭</Button>
       <Button type="primary" @click="handleOk">输出结果</Button>
@@ -9,9 +9,10 @@
 </template>
 <script lang="ts" setup>
 import KBuilder from '../../../../KBuilder'
-import { pluginManager } from '../../../../../utils'
+import { pluginManager, getUUID } from '../../../../../utils'
 import { ref, inject } from 'vue'
 import { NodeItem } from '../../../../../types/kDesigner'
+
 const Modal = pluginManager.getComponent('Modal')
 const Button = pluginManager.getComponent('button')
 
@@ -19,13 +20,15 @@ const visible = ref(false)
 const schemas = inject('schemas') as NodeItem[]
 const script = inject('script') as string
 const kb = ref<any>(null)
+const kBuilderKey = ref('')
 function handleClose () {
   visible.value = false
 }
 
 function handleOpen () {
-  console.log(Modal)
   visible.value = true
+  // 通过修改key重新渲染组件
+  kBuilderKey.value = getUUID()
 }
 
 async function handleOk () {
