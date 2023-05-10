a4b41a461f881da9932a3295dca9872d8b8e62cb
kcz <1150791676@qq.com>
2023-05-10 16:20:17 +0800

    docs: 更新文档

--- examples/main.ts
+++ examples/main.ts
@@ -8,10 +8,10 @@ import { pluginManager } from "../packages/index";
 
 
 // 引入Element plus样式
-// import "element-plus/dist/index.css";
-// import { setupElementPlus } from '../packages/ui/elementPlus'
-// // 使用Elemnt UI
-// setupElementPlus(pluginManager);
+import "element-plus/dist/index.css";
+import { setupElementPlus } from '../packages/ui/elementPlus'
+// 使用Elemnt UI
+setupElementPlus(pluginManager);
 
 
 // // 引入antd UI样式
@@ -21,8 +21,8 @@ import { pluginManager } from "../packages/index";
 // setupAntd(pluginManager);
 
 
-import { setupNaiveUi } from '../packages/ui/naiveUi'
-// 使用NaiveUi UI
-setupNaiveUi(pluginManager);
+// import { setupNaiveUi } from '../packages/ui/naiveUi'
+// // 使用NaiveUi UI
+// setupNaiveUi(pluginManager);
 
 createApp(App).mount("#app");
