d5aac3fb48e6635553706b52f30d46111c2c7b9c
kangcz <1150791676@qq.com>
2023-07-01 18:07:29 +0800

    chore: examples引入基础组件安装源码

--- packages/core/examples/main.ts
+++ packages/core/examples/main.ts
@@ -5,15 +5,16 @@ import App from './App.vue'
 import { pluginManager } from '../packages/index'
 // 使用Elemnt UI
 import 'element-plus/dist/index.css'
-import { setupElementPlus } from '@k-designer/ui'
+import { setupElementPlus } from '@k-designer/ui/elementPlus'
 setupElementPlus(pluginManager)
 
 // // 使用Antd UI
 // import 'ant-design-vue/dist/antd.css'
-// import { setupAntd } from '@k-designer/ui'
+// import { setupAntd } from '@k-designer/ui/antd'
 // setupAntd(pluginManager)
+
 // // 使用NaiveUi UI
-// import { setupNaiveUi } from '@k-designer/ui'
+// import { setupNaiveUi } from '@k-designer/ui/naiveUi'
 // setupNaiveUi(pluginManager)
 
 createApp(App).mount('#app')
