3a7441c7b7bde3dc3a65616e99f3c1f3808aa510
996427059 <a996427059@163.com>
2023-07-01 15:11:24 +0800

    fix:修复elementPlus 文件上传没有上传按钮的bug

--- packages/ui/elementPlus/upload-file/uploadFile.ts
+++ packages/ui/elementPlus/upload-file/uploadFile.ts
@@ -1,46 +1,113 @@
-import { type ComponentConfigModel } from '@k-designer/utils/pluginManager'
-export default {
-  component: async () => await import('./uploadFile'),
-  defaultSchema: {
-    label: '上传文件',
-    type: 'upload-file',
-    icon: 'icon-number',
-    field: 'uploadFile',
-    input: true,
-    componentProps: {
-      action: 'http://cdn.kcz66.com/upload-img.txt'
+import { type PropType, defineComponent, h, nextTick, computed, ref, watch } from 'vue'
+import { ElMessage, ElUpload, ElButton, type UploadProps, type UploadUserFile, type UploadRawFile } from 'element-plus'
+export default defineComponent({
+  props: {
+    modelValue: {
+      type: Array as PropType<UploadUserFile[]>,
+      default: () => []
     }
   },
-  config: {
-    attribute: [
-      {
-        label: '字段名',
-        type: 'input',
-        field: 'field'
+  emits: ['update:modelValue'],
+  setup (props, { emit, attrs }) {
+    const fileList = ref<UploadUserFile[]>([])
+    watch(fileList, (e) => {
+      emit('update:modelValue', e)
+    })
+    // 处理传递进来的值
+    watch(
+      () => props.modelValue,
+      (e) => {
+        if ((e != null) && e.length > 0 && (fileList.value != null)) {
+          // props modelValue 等于 data 不进行处理
+          if (fileList.value === e) return
+          fileList.value.length = 0
+          fileList.value.push(...e)
+        }
       },
-      {
-        label: '文字',
-        type: 'input',
-        field: 'label'
-      },
-      {
-        label: '隐藏',
-        type: 'switch',
-        field: 'componentProps.hidden'
-      },
-      {
-        label: '禁用',
-        type: 'switch',
-        field: 'componentProps.disabled'
-      },
-      {
-        label: '表单校验',
-        type: 'KRuleEditor',
-        layout: 'vertical',
-        field: 'rules',
-        describe: '校验规则需要配合表单使用'
-      }
-    ]
-  },
-  bindModel: 'modelValue'
-} as ComponentConfigModel
+      { deep: true, immediate: true }
+    )
+
+    // function handleChange : UploadProps['onChange']  (e: UploadUserFile[]) => {
+    //   nextTick(() => { fileList.value = e })
+    // }
+    const handleChange: UploadProps['onChange'] = (uploadFile, uploadFiles): void => {
+      nextTick(() => { fileList.value = uploadFiles })
+    }
+    // 处理数据结果
+    // const handleChange = (info: UploadChangeParam): void => {
+    //   if (info.file.status === 'uploading') {
+    //     return
+    //   }
+    //
+    //   if (info.file.status === 'done') {
+    //     // Get this url from response in real world.
+    //     const url = info.file.response?.data?.url
+    //     if (!info.file.url && !url) {
+    //       info.file.status = 'error'
+    //       ElMessage.error('上传失败')
+    //       return
+    //     }
+    //     // 赋值url
+    //     info.file.url = url
+    //   }
+    //
+    //   if (info.file.status === 'error') {
+    //     ElMessage.error('upload error')
+    //   }
+    // }
+
+    // 上传前处理
+    const beforeUpload: UploadProps['beforeUpload'] = (file: UploadRawFile): void => {
+      // const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+      // if (!isJpgOrPng) {
+      //   message.error('您只能上传JPG/PNG文件!');
+      // }
+      // const isLt2M = file.size / 1024 / 1024 < 2;
+      // if (!isLt2M) {
+      //   message.error('图片大小超过 2MB!');
+      // }
+      // return isJpgOrPng && isLt2M;
+    }
+    const handleSuccess: UploadProps['onSuccess'] = (response, uploadFile, uploadFiles) => {
+      console.log(uploadFiles)
+    }
+    const handleError: UploadProps['onError'] = (error, uploadFile, uploadFiles) => {
+      ElMessage.error('上传失败')
+      console.error(error)
+    }
+    // ts报错先忽略了
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const getUploadProps = computed<UploadProps>(() => ({
+      ...attrs,
+      onBeforeUpload: beforeUpload,
+      onChange: handleChange,
+      onSuccess: handleSuccess,
+      onError: handleError
+    }))
+
+    return () => {
+      return h('div', null, {
+        default: () => [
+          h(ElUpload, getUploadProps.value, {
+            default: () => [
+              h(
+                ElButton,
+                {},
+                {
+                  default: () => [
+                    h('span', {
+                      class: 'iconfont icon-shangchuan1',
+                      style: { 'margin-right': '2px' }
+                    }),
+                    h('span', null, { default: () => '上传文件' })
+                  ]
+                }
+              )
+            ]
+          })
+        ]
+      })
+    }
+  }
+})
