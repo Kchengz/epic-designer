2ff0dcb16b75019958fa39f33d0036b173f4b41f
996427059 <a996427059@163.com>
2023-07-13 10:16:54 +0800

    feat: 补充antDesigner/cascader组件属性

--- packages/ui/antd/cascader/index.ts
+++ packages/ui/antd/cascader/index.ts
@@ -6,7 +6,20 @@ export default {
     type: 'cascader',
     field: 'cascader',
     icon: 'icon-write',
-    input: true
+    input: true,
+    componentProps: {
+      options: [
+        {
+          label: "选项1",
+          value: "选项1",
+        },
+        {
+          label: "选项2",
+          value: "选项2",
+        },
+      ],
+      placeholder: "请选择",
+    }
   },
   config: {
     attribute: [
@@ -26,26 +39,51 @@ export default {
         field: 'componentProps.defaultValue'
       },
       {
-        label: '输入类型',
+        label: '多选',
+        type: 'switch',
+        field: 'componentProps.multiple'
+      },
+      {
+        label: '可搜索',
+        type: 'switch',
+        field: 'componentProps.showSearch'
+      },
+      {
+        label: '最大tag文本长度',
+        type: 'number',
+        field: 'componentProps.maxTagTextLength',
+        show: ({values})=>values.componentProps.multiple
+      },
+      {
+        label: '最大tag显示数',
+        type: 'number',
+        field: 'componentProps.maxTagCount',
+        show: ({values})=>values.componentProps.multiple
+      },
+      {
+        label: '弹出框位置',
         type: 'select',
-        defaultValue: 'text',
         componentProps: {
           options: [
             {
-              label: 'text',
-              value: 'text'
+              label: 'bottomLeft',
+              value: 'bottomLeft'
             },
             {
-              label: 'number',
-              value: 'number'
+              label: 'bottomRight',
+              value: 'bottomRight'
             },
             {
-              label: 'password',
-              value: 'password'
+              label: 'topLeft',
+              value: 'topLeft'
+            },
+            {
+              label: 'topRight',
+              value: 'topRight'
             }
           ]
         },
-        field: 'componentProps.type'
+        field: 'componentProps.placement'
       },
       {
         label: '可清空',
@@ -68,6 +106,16 @@ export default {
         layout: 'vertical',
         field: 'rules',
         describe: '校验规则需要配合表单使用'
+      },
+      {
+        label: "选项管理",
+        type: "KOptionsEditor",
+        layout: "vertical",
+        field: "componentProps.options",
+        describe: "校验规则需要配合表单使用",
+        componentProps: {
+          tree: true
+        }
       }
     ]
   },
