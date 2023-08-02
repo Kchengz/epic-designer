3da1b5a59880ceb7ec5d9c41164b1673e1cfed45
kangcz <1150791676@qq.com>
2023-08-02 23:23:09 +0800

    feat: antdUI日期选择器增加时间选择配置

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -3,14 +3,14 @@ import './style.css'
 import App from './App.vue'
 import { pluginManager } from '@epic-designer/core'
 // // // 使用Elemnt UI
-import 'element-plus/dist/index.css'
-import { setupElementPlus } from '@epic-designer/ui/elementPlus'
-setupElementPlus(pluginManager)
+// import 'element-plus/dist/index.css'
+// import { setupElementPlus } from '@epic-designer/ui/elementPlus'
+// setupElementPlus(pluginManager)
 
 // 使用Antd UI
-// import 'ant-design-vue/dist/antd.css'
-// import { setupAntd } from '@epic-designer/ui/antd'
-// setupAntd(pluginManager)
+import 'ant-design-vue/dist/antd.css'
+import { setupAntd } from '@epic-designer/ui/antd'
+setupAntd(pluginManager)
 
 // 使用NaiveUi UI
 // import { setupNaiveUi } from '@epic-designer/ui/naiveUi'
