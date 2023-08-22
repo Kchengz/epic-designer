119296366acd502bbcabf26af20fda32e8846b88
kangcz <1150791676@qq.com>
2023-08-22 21:26:37 +0800

    chore: 校验必填规制添加类型设置

--- packages/core/extensions/ERuleEditor/ERuleItem.vue
+++ packages/core/extensions/ERuleEditor/ERuleItem.vue
@@ -1,34 +1,19 @@
 <template>
   <div
-    class="rule-item-main bg-white m-t-2 p-2 rounded  border border-solid border-gray-200 hover:border-primary transition-all relative"
-  >
-    <template
-      v-for="(record, index) in ruleItemSchemas"
-      :key="index"
-    >
-      <div
-        v-if="record.show ? record.show() : true"
-        class="flex m-t-2 first:m-0"
-      >
-        <div
-          class="attr-label"
-          title="校验时机"
-        >
+    class="rule-item-main bg-white m-t-2 p-2 rounded  border border-solid border-gray-200 hover:border-primary transition-all relative">
+    <template v-for="(record, index) in ruleItemSchemas" :key="index">
+      <div v-if="record.show ? record.show() : true" class="flex m-t-2 first:m-0">
+        <div class="attr-label" title="校验时机">
           {{ record.label }}
         </div>
         <div class="attr-input">
-          <ENode
-            v-model="modelRule[record.model]"
-            :record="{ ...record, noFormItem: true }"
-            @change="handleUpdate"
-          />
+          <ENode v-model="modelRule[record.model]" :record="{ ...record, noFormItem: true }" @change="handleUpdate" />
         </div>
       </div>
     </template>
     <div
       class="rule-btn-delete absolute top-0 right-0 transition-all w-6 h-6 cursor-pointer rounded-bl-1 flex-center color-white"
-      @click="handleDelete"
-    >
+      @click="handleDelete">
       <span class="iconfont icon-shanchu1" />
     </div>
   </div>
@@ -38,7 +23,8 @@ import { FormItemRule } from './types'
 import { computed, inject } from 'vue'
 import { PageManager } from '@epic-designer/utils'
 import ENode from '../../components/node/index'
-const emit = defineEmits(['change', 'delete','update:rule'])
+import { typeOptions, triggerOptions, lenTypeOptions } from './data'
+const emit = defineEmits(['change', 'delete', 'update:rule'])
 const props = defineProps<{
   rule: FormItemRule,
 }>()
@@ -60,26 +46,6 @@ const methodOptions = computed(() => {
     .map(([label]) => ({ label, value: label }))
 })
 
-const lenTypeOptions = ['string', 'number', 'url', 'array', 'email']
-const triggerOptions = [{ label: 'change', value: 'change' }, { label: 'blur', value: 'blur' }]
-const typeOptions = [
-  { label: 'string', value: 'string' },
-  { label: 'number', value: 'number' },
-  { label: 'boolean', value: 'boolean' },
-  { label: 'method', value: 'method' },
-  { label: 'regexp', value: 'regexp' },
-  { label: 'integer', value: 'integer' },
-  { label: 'float', value: 'float' },
-  { label: 'array', value: 'array' },
-  { label: 'object', value: 'object' },
-  // { label: 'enum', value: 'enum' },
-  { label: 'date', value: 'date' },
-  { label: 'url', value: 'url' },
-  { label: 'hex', value: 'hex' },
-  { label: 'email', value: 'email' },
-  { label: 'any', value: 'any' }
-]
-
 const ruleItemSchemas = [
   {
     type: 'select',
@@ -96,42 +62,42 @@ const ruleItemSchemas = [
     type: 'select',
     label: '校验函数',
     model: 'validator',
-    show () { return Boolean(modelRule.value.isValidator) },
+    show() { return Boolean(modelRule.value.isValidator) },
     componentProps: { options: methodOptions.value, placeholder: '校验函数' }
   },
   {
     type: 'select',
     label: '类型',
     model: 'type',
-    show () { return !modelRule.value.isValidator },
+    show() { return !modelRule.value.isValidator },
     componentProps: { options: typeOptions, placeholder: '类型' }
   },
   {
     type: 'input',
     label: '正则校验',
     model: 'pattern',
-    show () { return !modelRule.value.isValidator },
+    show() { return !modelRule.value.isValidator },
     componentProps: { placeholder: '正则校验' }
   },
   {
     type: 'number',
     label: '字段长度',
     model: 'len',
-    show () { return lenTypeOptions.includes(modelRule.value.type ?? '') },
+    show() { return lenTypeOptions.includes(modelRule.value.type ?? '') },
     componentProps: { min: 0, placeholder: '字段长度' }
   },
   {
     type: 'number',
     label: '最小长度',
     model: 'min',
-    show () { return lenTypeOptions.includes(modelRule.value.type ?? '') },
+    show() { return lenTypeOptions.includes(modelRule.value.type ?? '') },
     componentProps: { min: 0, placeholder: '最小长度' }
   },
   {
     type: 'number',
     label: '最大长度',
     model: 'max',
-    show () { return lenTypeOptions.includes(modelRule.value.type ?? '') },
+    show() { return lenTypeOptions.includes(modelRule.value.type ?? '') },
     componentProps: { min: 0, placeholder: '最大长度' }
   },
   {
@@ -145,7 +111,7 @@ const ruleItemSchemas = [
 /**
  * 更新校验规则
  */
-function handleUpdate () {
+function handleUpdate() {
   const v = modelRule.value
   if (v.isValidator) {
     delete v.type
@@ -163,7 +129,7 @@ function handleUpdate () {
 /**
  * 删除校验规则
  */
-function handleDelete () {
+function handleDelete() {
   emit('delete')
 }
 
--- packages/core/extensions/ERuleEditor/index.vue
+++ packages/core/extensions/ERuleEditor/index.vue
@@ -1,40 +1,22 @@
 <template>
   <div>
     <div
-      class="rule-item-main bg-white m-t-2 p-2 rounded  border border-solid border-gray-200 hover:border-primary transition-all relative"
-    >
-      <template
-        v-for="(record, index) in requiredRuleSchemas"
-        :key="index"
-      >
-        <div
-          v-if="record.show ? record.show() : true"
-          class="flex m-t-2 first:m-0"
-        >
+      class="rule-item-main bg-white m-t-2 p-2 rounded  border border-solid border-gray-200 hover:border-primary transition-all relative">
+      <template v-for="(record, index) in requiredRuleSchemas" :key="index">
+        <div v-if="record.show ? record.show() : true" class="flex m-t-2 first:m-0">
           <div class="attr-label">
             {{ record.label }}
           </div>
           <div class="flex-1">
-            <ENode
-              v-model="requiredRule[record.model]"
-              :record="{ ...record, noFormItem: true }"
-              @change="handleUpdate"
-            />
+            <ENode v-model="requiredRule[record.model]" :record="{ ...record, noFormItem: true }"
+              @change="handleUpdate" />
           </div>
         </div>
       </template>
     </div>
-    <ERuleItem
-      v-for="(item, index) in rules"
-      :key="index"
-      v-model:rule="rules[index]"
-      @delete="handleDelete(index)"
-      @change="handleUpdate"
-    />
-    <Button
-      class="m-t-2"
-      @click="handleAdd"
-    >
+    <ERuleItem v-for="(item, index) in rules" :key="index" v-model:rule="rules[index]" @delete="handleDelete(index)"
+      @change="handleUpdate" />
+    <Button class="m-t-2" @click="handleAdd">
       添加规则
     </Button>
   </div>
@@ -45,6 +27,7 @@ import { ref, watch, PropType } from 'vue'
 import { FormItemRule } from './types'
 import ERuleItem from './ERuleItem.vue'
 import ENode from '../../components/node/index'
+import { triggerOptions, typeOptions } from './data'
 const Button = pluginManager.getComponent('button')
 const props = defineProps({
   modelValue: {
@@ -52,34 +35,58 @@ const props = defineProps({
     default: undefined
   }
 })
+
+
 const requiredRule = ref<FormItemRule>({
   required: false,
-  message: '必填项',
-  trigger: ['change']
-})
+  message: "必填项",
+  type: "string",
+  trigger: ["change"],
+});
 
-const triggerOptions = [{ label: 'change', value: 'change' }, { label: 'blur', value: 'blur' }]
 const requiredRuleSchemas = [
   {
-    type: 'switch',
-    label: '必填项',
-    model: 'required'
+    type: "switch",
+    label: "必填项",
+    model: "required",
+  },
+  {
+    type: "select",
+    label: "校验时机",
+    model: "trigger",
+    show() {
+      return Boolean(requiredRule.value.required);
+    },
+    componentProps: {
+      options: triggerOptions,
+      placeholder: "校验时机",
+      multiple: true,
+      mode: "multiple",
+    },
   },
   {
     type: 'select',
-    label: '校验时机',
-    model: 'trigger',
-    show () { return Boolean(requiredRule.value.required) },
-    componentProps: { options: triggerOptions, placeholder: '校验时机', multiple: true, mode: 'multiple' }
+    label: '类型',
+    model: 'type',
+    show() {
+      return Boolean(requiredRule.value.required);
+    },
+    componentProps: { options: typeOptions, placeholder: '类型' }
   },
   {
-    type: 'input',
-    label: '校验信息',
-    model: 'message',
-    show () { return Boolean(requiredRule.value.required) },
-    componentProps: { placeholder: '校验信息' }
-  }
-]
+    type: "input",
+    label: "校验信息",
+    model: "message",
+    show() {
+      return Boolean(requiredRule.value.required);
+    },
+    componentProps: { placeholder: "校验信息" },
+  },
+];
+
+
+
+
 const rules = ref<FormItemRule[]>([])
 const emit = defineEmits(['update:modelValue'])
 
@@ -103,7 +110,7 @@ watch(() => props.modelValue, (e) => {
 /**
  * 新增检验规则
  */
-function handleAdd () {
+function handleAdd() {
   rules.value.push({
     message: '',
     trigger: ['change']
@@ -114,7 +121,7 @@ function handleAdd () {
 /**
  * 更新校验规则
  */
-function handleUpdate () {
+function handleUpdate() {
   // 存在必填项时,合并其他规则
   if (requiredRule.value.required) {
     emit('update:modelValue', deepClone([...rules.value, requiredRule.value]))
@@ -135,7 +142,7 @@ function handleUpdate () {
  * 通过下标删除校验规则项
  * @param index
  */
-function handleDelete (index: number) {
+function handleDelete(index: number) {
   rules.value.splice(index, 1)
   handleUpdate()
 }
