128358f58d7daed2f5894b0f0fbfaa9587aaf5a1
kcz <1150791676@qq.com>
2023-11-06 22:47:38 +0800

    chore: 更新demo示例

--- examples/src/views/builder/index.vue
+++ examples/src/views/builder/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <EBuilder ref="kfb" :pageSchema="{ schemas }">
+    <EBuilder ref="kfb" :pageSchema="pageSchema">
       <template #sdff="{ model, record }">
         <input v-model="model[record.field]">
       </template>
@@ -15,81 +15,101 @@
 import { ref, onMounted } from 'vue'
 import { EBuilder } from '@epic-designer/core'
 
-import { NodeItem } from '@epic-designer/core/types/epic-designer'
+import { PageSchema } from '@epic-designer/core/types/epic-designer'
 
 const kfb = ref<any>(null)
 async function handleGetData() {
   const values = await kfb.value?.getData()
   console.log(values)
 }
-const schemas: NodeItem[] = [{
-  type: 'form',
-  id: 'root',
-  name: 'default',
-  componentProps: {
-    layout: 'inline',
-    labelWidth: 80,
-    labelLayout: 'fixed',
-    labelCol: {
-      span: 5
-    },
-    wrapperCol: {
-      span: 19
-    },
-    hideRequiredMark: false
-  },
-  children: [
+const pageSchema = ref<PageSchema>({
+  "schemas": [
     {
-      label: '输入框',
-      type: 'slot',
-      field: 'slotText',
-      slotName: 'sdff',
-      icon: 'icon-write',
-      input: true,
-      id: '6jewjjk2jho00',
-      componentProps: {
-        type: 'text'
-      }
-    },
-    {
-      label: '密码输入框',
-      type: 'number',
-      icon: 'icon-number',
-      field: 'number',
-      input: true,
-      show: ({ values }) => {
-        return values.input !== '4444'
-      },
-      rules: [
+      "type": "page",
+      "id": "root",
+      "label": "页面",
+      "children": [
         {
-          require: true,
-          message: '请输入'
+          "label": "表单",
+          "type": "form",
+          "icon": "icon-daibanshixiang",
+          "name": "default",
+          "componentProps": {
+            "labelWidth": 100,
+            "labelCol": {
+              "span": 5
+            },
+            "wrapperCol": {
+              "span": 19
+            },
+            "hideRequiredMark": false,
+            "labelPosition": "right",
+            "size": "default",
+            "requireAsteriskPosition": "left"
+          },
+          "children": [
+            {
+              "label": "输入框",
+              "type": "input",
+              "field": "input_tzpfvoqp",
+              "icon": "icon-write",
+              "input": true,
+              "componentProps": {
+                "defaultValue": "",
+                "placeholder": "请输入",
+                "size": "default",
+                "type": "text"
+              },
+              "id": "input_tzpfvoqp"
+            },
+            {
+              "label": "选择框",
+              "type": "select",
+              "icon": "icon-xiala",
+              "field": "select_gbqzlhlu",
+              "input": true,
+              "componentProps": {
+                "options": [
+                  {
+                    "label": "选项1",
+                    "value": "选项1"
+                  },
+                  {
+                    "label": "选项2",
+                    "value": "选项2"
+                  }
+                ],
+                "placeholder": "请选择",
+                "size": "default",
+                "effect": "light",
+                "placement": "bottom-start"
+              },
+              "id": "select_gbqzlhlu"
+            }
+          ],
+          "id": "form_ydqzoj6x"
         }
       ],
-      dataSource: {
-        api: () => {
-          return [{
-            label: '23423',
-            value: 1212
-          }]
+      "componentProps": {
+        "style": {
+          "padding": "16px"
         }
-      },
-      id: '8gdrepx5ep800'
+      }
     }
-  ]
-}]
+  ],
+  "script": "const { defineExpose, getComponent } = this;\n\nfunction test (){\n    console.log('test')\n}\n\n// 通过defineExpose暴露的函数或者属性\ndefineExpose({\n test \n})"
+})
+
 
 // 表单赋值
 onMounted(async () => {
   kfb.value?.setData({
-    number: '234',
-    slotText: "234234"
+    input_tzpfvoqp: '测试数据'
   })
 
-  const data = await kfb.value?.getData()
-  const data1 = await kfb.value?.validate()
+  // const data = await kfb.value?.getData()
+  const data = await kfb.value?.validate()
   console.log(data)
-  console.log(data1,'---')
 })
 
 
