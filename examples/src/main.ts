0c0c90cb21941e3b5a8000bb62063199c1f32a71
kcz <1150791676@qq.com>
2023-11-26 01:13:19 +0800

    build: 📦发布v0.8.14

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -8,14 +8,14 @@ import { pluginManager } from '@epic-designer/core'
 // setupElementPlus(pluginManager)
 
 // 使用Antd UI v4版本
-// import './style/reset.css';
-// import { setupAntd } from '@epic-designer/ui/antd'
-// setupAntd(pluginManager)
+import './style/reset.css';
+import { setupAntd } from '@epic-designer/ui/antd'
+setupAntd(pluginManager)
 
 // 使用Antd UI v3 版本
-import 'ant-design-vue/dist/antd.css'
-import { setupAntdV3 } from '@epic-designer/ui-antdv3'
-setupAntdV3(pluginManager)
+// import 'ant-design-vue/dist/antd.css'
+// import { setupAntdV3 } from '@epic-designer/ui-antdv3'
+// setupAntdV3(pluginManager)
 
 // 使用NaiveUi UI
 // import { setupNaiveUi } from '@epic-designer/ui/naiveUi'
