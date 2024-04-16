3025bc33edc5fa3b4053c952ae39294d3a55323e
kcz <1150791676@qq.com>
2024-04-16 13:01:54 +0800

    fix: 修复antdUI4 Modal组件警告问题

--- packages/ui/antd/modal/modal.vue
+++ packages/ui/antd/modal/modal.vue
@@ -7,15 +7,15 @@
         </template>
         <slot v-else></slot>
       </slot>
-
     </div>
     <div class="epic-modal-footer">
       <Space align="end">
         <Button @click="handleClose">关闭</Button>
-        <Button type="primary" @click="handleOk">{{ componentSchema.okText ?? "确定" }}</Button>
+        <Button type="primary" @click="handleOk">{{
+          componentSchema.okText ?? "确定"
+        }}</Button>
       </Space>
     </div>
-
   </Modal>
 </template>
 
@@ -23,6 +23,11 @@
 import { computed, h, type PropType, useAttrs } from "vue";
 import { type ComponentSchema } from "@epic-designer/core/types/epic-designer";
 import { Modal, Button, Space } from "ant-design-vue";
+import { version } from "ant-design-vue";
+  const versionArray = version.split(".");
+  // 获取版本号第一个数字
+  const firstNumber = parseInt(versionArray[0]);
+
 const attrs = useAttrs()
 const props = defineProps({
   componentSchema: {
@@ -54,7 +59,7 @@ const componentSchema = computed<Record<string, any>>(() => ({
   title: props.componentSchema?.label ?? "",
   wrapClassName: "epic-modal-ant",
   open: attrs.modelValue,
-  visible: attrs.modelValue,
+  visible: firstNumber > 3 ? undefined : attrs.modelValue,
   "onUpdate:open": handleClose,
   "onUpdate:visible": handleClose,
   style: "top:20px",
