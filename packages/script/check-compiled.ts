0df5660b3687ce42b88257f66dde408fadbaf815
Kchengz <1150791676@qq.com>
2024-04-18 23:50:21 +0800

    build: 📦发布v0.9.8版本

--- packages/script/check-compiled.ts
+++ packages/script/check-compiled.ts
@@ -6,15 +6,14 @@ const filePath: string =  path.join(__dirname, "../epic-designer/dist/index.js")
 
 
 if (fs.existsSync(filePath)) {
-  console.log("恭喜满足启动条件，正在启动...");
+  console.log("已满足启动条件，正在启动...");
 } else {
   console.log("引用文件未编译，正在编译请稍等...");
-  exec("cd ../../ && npm run lib", (error: ExecException | null, stdout: string, stderr: string) => {
+  exec("cd ../../ && npm run lib", (error: ExecException | null) => {
     if (error) {
       console.error(`执行 npm lib 时出错: ${error}`);
       return;
     }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
+    console.log(`编译完成`);
   });
 }
