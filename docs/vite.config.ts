2658eade5379a201e116d04159bc621657ab4694
Kchengz <1150791676@qq.com>
2024-05-14 13:45:59 +0800

    fix: 修复文档无法编译问题

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
 import UnoCSS from "unocss/vite";
-import monacoEditorPlugin from "vite-plugin-monaco-editor";
+// import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 export default defineConfig({
   plugins: [
     UnoCSS(),
-    (monacoEditorPlugin as any).default({
-      languageWorkers: [
-        "editorWorkerService",
-        "css",
-        "html",
-        "json",
-        "typescript",
-      ],
-    }),
+    // (monacoEditorPlugin as any).default({
+    //   languageWorkers: [
+    //     "editorWorkerService",
+    //     "css",
+    //     "html",
+    //     "json",
+    //     "typescript",
+    //   ],
+    // }),
   ],
 });
