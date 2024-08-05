ee1e3f476b8c7866c6a157b3462b932710bade7b
Kchengz <1150791676@qq.com>
2024-08-05 09:49:57 +0800

    fix: 🐛修复getUUID函数无法通过生成长度测试问题

--- packages/utils/common/string.ts
+++ packages/utils/common/string.ts
@@ -3,9 +3,15 @@
  * @param randomLength 随机id长度 0 - 11
  */
 export function getUUID(randomLength = 10): string {
-  return Math.random()
-    .toString(36)
-    .substring(2, 2 + randomLength);
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let uuid = '';
+  
+  for (let i = 0; i < randomLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    uuid += characters[randomIndex];
+  }
+
+  return uuid;
 }
 
 /**
