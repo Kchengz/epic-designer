bc7de6682fdef97e39faf3c96634c297768f6a7f
kcz <1150791676@qq.com>
2024-08-06 21:09:27 +0800

    chore: 动作配置组件大纲显示组件id

--- packages/core/components/designer/src/modules/outline/outline.vue
+++ packages/core/components/designer/src/modules/outline/outline.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="epic-outline">
-    <ETree :options="pageSchema.schemas" draggable :selected-keys="selectedKeys" :hover-key="designer.state.hoverNode?.id"
-      @nodeClick="handleNodeClick">
+    <ETree
+      :options="pageSchema.schemas"
+      draggable
+      :selected-keys="selectedKeys"
+      :hover-key="designer.state.hoverNode?.id"
+      @nodeClick="handleNodeClick"
+    >
       <template #tree-node="{ schema }">
-        <div class="epic-text-padding truncate" @mouseenter.stop="designer.setHoverNode(schema)"
-          @mouseleave.stop="designer.setHoverNode(null)">
-          {{ schema.label ??
-            pluginManager.getComponentConfingByType(schema.type)?.defaultSchema.label }}
-            <span class="epic-node-type-text">
+        <div
+          class="epic-text-padding flex"
+          @mouseenter.stop="designer.setHoverNode(schema)"
+          @mouseleave.stop="designer.setHoverNode(null)"
+        >
+          <span class="max-w-full truncate">
+            {{
+              schema.label ??
+              pluginManager.getComponentConfingByType(schema.type)?.defaultSchema.label
+            }}
+          </span>
+          <span class="epic-node-type-text flex-1 w-0 truncate">
             {{ schema.id }}
           </span>
         </div>
@@ -16,24 +28,22 @@
   </div>
 </template>
 <script lang="ts" setup>
-import ETree from '../../../../tree'
-import { inject, computed } from 'vue'
-import { PageSchema, Designer } from '../../../../../types/epic-designer'
-import { pluginManager } from '@epic-designer/utils'
+import ETree from "../../../../tree";
+import { inject, computed } from "vue";
+import { PageSchema, Designer } from "../../../../../types/epic-designer";
+import { pluginManager } from "@epic-designer/utils";
 
-const designer = inject('designer') as Designer
-const pageSchema = inject('pageSchema') as PageSchema
-
+const designer = inject("designer") as Designer;
+const pageSchema = inject("pageSchema") as PageSchema;
 
 // 计算选中节点值
 const selectedKeys = computed(() => {
-  const id = designer.state.checkedNode?.id
-  return id ? [id] : []
-})
+  const id = designer.state.checkedNode?.id;
+  return id ? [id] : [];
+});
 
 // 设置选中节点
 function handleNodeClick(e: any) {
-  designer.setCheckedNode(e.componentSchema)
+  designer.setCheckedNode(e.componentSchema);
 }
-
 </script>
--- packages/core/components/tree/src/treeNodeItem.vue
+++ packages/core/components/tree/src/treeNodeItem.vue
@@ -1,87 +1,117 @@
 <template>
-  <li class="epic-tree-node"
-    :class="{ expanded: props.schema.children?.length, 'level-1': props.schema.type === 'page' }">
+  <li
+    class="epic-tree-node"
+    :class="{
+      expanded: props.schema.children?.length,
+      'level-1': props.schema.type === 'page',
+    }"
+  >
     <a>
-      <span v-if="props.schema.children?.length && props.schema.type !== 'page'" class="icon-expanded"
-        :class="{ expanded }" @click="handleExpanded">
+      <span
+        v-if="props.schema.children?.length && props.schema.type !== 'page'"
+        class="icon-expanded"
+        :class="{ expanded }"
+        @click="handleExpanded"
+      >
         <EIcon name="icon--epic--caret-right-outlined" />
       </span>
       <TreeNodeText />
     </a>
-    <ETreeNodes v-if="props.schema.children?.length" class="epic-tree-sublist" :class="{ expanded }"
-      v-model:schemas="props.schema.children" :parentSchema="props.schema"  />
+    <ETreeNodes
+      v-if="props.schema.children?.length"
+      class="epic-tree-sublist"
+      :class="{ expanded }"
+      v-model:schemas="props.schema.children"
+      :parentSchema="props.schema"
+    />
   </li>
 </template>
 <script lang="ts" setup>
-import { ComponentSchema } from '../../../types/epic-designer'
-import EIcon from '../../icon'
-import { inject, computed, Ref, h, defineComponent, Slots } from 'vue'
-import { pluginManager } from '@epic-designer/utils'
-import ETreeNodes from './treeNodes.vue'
+import { ComponentSchema } from "../../../types/epic-designer";
+import EIcon from "../../icon";
+import { inject, computed, Ref, h, defineComponent, Slots } from "vue";
+import { pluginManager } from "@epic-designer/utils";
+import ETreeNodes from "./treeNodes.vue";
 defineOptions({
-  name: 'ETreeNodeItem'
-})
+  name: "ETreeNodeItem",
+});
 
 const props = defineProps<{
-  schema: ComponentSchema
-}>()
+  schema: ComponentSchema;
+}>();
 
+const slots = inject("slots", {}) as Slots;
 
-const slots = inject('slots', {}) as Slots
+const expandedKeys = inject("expandedKeys") as Ref<string[]>;
+const treeProps = inject("treeProps") as any;
+const selectedKeys = inject("selectedKeys") as Ref<string[]>;
 
-const expandedKeys = inject('expandedKeys') as Ref<string[]>
-const treeProps = inject('treeProps') as any
-const selectedKeys = inject('selectedKeys') as Ref<string[]>
-
-const handleSelect = inject('handleSelect') as (id: string, componentSchema: ComponentSchema) => {}
+const handleSelect = inject("handleSelect") as (
+  id: string,
+  componentSchema: ComponentSchema
+) => {};
 
 const expanded = computed(() => {
-  return expandedKeys.value.includes(props.schema.id ?? '')
-})
-
+  return expandedKeys.value.includes(props.schema.id ?? "");
+});
 
 const TreeNodeText = defineComponent({
   setup() {
-    return () => h('span', {
-      class: {
-        text: true,
-        hover: treeProps.hoverKey === props.schema.id,
-        checked: selectedKeys.value.includes(props.schema.id!)
-      },
-      onClick: () => handleSelect(props.schema.id!, props.schema)
-    },
-      slots['tree-node']?.(props) ??
-      h('span', { class: 'epic-text-padding' },
+    return () =>
+      h(
+        "span",
         {
-          default: () => [
-            props.schema.label ?? pluginManager.getComponentConfingByType(props.schema.type)?.defaultSchema.label,
-            h('span', { class: 'epic-node-type-text' }, props.schema.type)
-          ]
-        }
-      )
-    )
-  }
-})
+          class: {
+            text: true,
+            hover: treeProps.hoverKey === props.schema.id,
+            checked: selectedKeys.value.includes(props.schema.id!),
+          },
+          onClick: () => handleSelect(props.schema.id!, props.schema),
+        },
+        slots["tree-node"]?.(props) ??
+          h(
+            "span",
+            { class: "epic-text-padding flex" },
+            {
+              default: () => [
+                h(
+                  "span",
+                  { class: "max-w-full truncate" },
+                  props.schema.label ??
+                    pluginManager.getComponentConfingByType(props.schema.type)
+                      ?.defaultSchema.label
+                ),
+                h(
+                  "span",
+                  { class: "epic-node-type-text flex-1 w-0 truncate" },
+                  props.schema.id
+                ),
+              ],
+            }
+          )
+      );
+  },
+});
 
 function handleExpanded() {
-  const id = props.schema.id
+  const id = props.schema.id;
   if (!id) {
-    return false
+    return false;
   }
 
   if (expandedKeys.value.includes(id)) {
-    expandedKeys.value = expandedKeys.value.filter(item => item !== id)
+    expandedKeys.value = expandedKeys.value.filter((item) => item !== id);
   } else {
-    expandedKeys.value.push(id)
+    expandedKeys.value.push(id);
   }
 }
 
 function init() {
-  const id = props.schema.id
+  const id = props.schema.id;
   if (!id || !props.schema.children?.length) {
-    return false
+    return false;
   }
-  expandedKeys.value.push(id)
+  expandedKeys.value.push(id);
 }
-init()
+init();
 </script>
