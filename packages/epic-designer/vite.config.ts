f4c463578f53bdc852d53903a5f97abe3b64c1e9
kcz <1150791676@qq.com>
2024-12-22 00:23:11 +0800

    build: 调整编译文件位置，模块文件放入chunks文件夹

--- packages/epic-designer/vite.config.ts
+++ packages/epic-designer/vite.config.ts
@@ -35,14 +35,14 @@ export default defineConfig({
       // 指定组件编译入口文件
       name: "epic-designer",
       // formats: ["es"],
-      fileName: (ModuleFormat,entryName) => {
-        console.log(ModuleFormat,entryName)
-        const extension = ModuleFormat === 'es' ? 'js' : ModuleFormat
+      fileName: (ModuleFormat, entryName) => {
+        // console.log(ModuleFormat,entryName)
+        // const extension = ModuleFormat === 'es' ? 'js' : ModuleFormat
         // 区分默认入口文件和UI注册文件
         const isIndexEntry = entryName === 'index'
         const path = isIndexEntry
-          ? `index.${extension}`
-          : `ui/${entryName}/index.${extension}`
+          ? `index.${ModuleFormat}`
+          : `ui/${entryName}/index.${ModuleFormat}`
         return path
       }
     },
@@ -60,6 +60,25 @@ export default defineConfig({
         "dayjs"
       ],
       output: {
+        chunkFileNames: ({ moduleIds }) => {
+          // 将 moduleIds 转为一个字符串，以加速关键词匹配
+          const moduleIdString = moduleIds.join("|");
+
+          // 定义关键词和对应路径的映射
+          const modulePathMap = {
+            antd: "ui/antd/chunks/[name].js",
+            elementPlus: "ui/elementPlus/chunks/[name].js",
+            naiveUi: "ui/naiveUi/chunks/[name].js",
+          };
+
+          // 遍历映射表，找到匹配的路径
+          for (const [key, path] of Object.entries(modulePathMap)) {
+            if (moduleIdString.includes(key)) {
+              return path;
+            }
+          }
+          return `chunks/[name].js`;
+        },
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: "Vue",
@@ -67,11 +86,11 @@ export default defineConfig({
       },
       plugins: [
         rollupCopy({
-            targets: [{src: '../core/theme', dest: './dist/'}], // 路径
-            hook: 'writeBundle', // 钩子，插件运行在rollup完成打包并将文件写入磁盘之前
-            verbose: true // 在终端进行console.log
+          targets: [{ src: '../core/theme', dest: './dist/' }], // 路径
+          hook: 'writeBundle', // 钩子，插件运行在rollup完成打包并将文件写入磁盘之前
+          verbose: true // 在终端进行console.log
         })
-    ]
+      ]
     },
     commonjsOptions: {
       esmExternals: true,
