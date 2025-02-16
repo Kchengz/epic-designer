45e81e346aead819e6d39d4b9bd45b5d94aad697
kcz <1150791676@qq.com>
2025-02-16 23:50:00 +0800

    feat(cli):✨新增epic命令行工具，添加 checkCompiled 命令，检查编译状态

--- scripts/src/check-compiled/index.ts
+++ scripts/src/check-compiled/index.ts
@@ -1,22 +1,41 @@
 import fs from "fs";
 import { exec, ExecException } from "child_process";
 import path from "path";
+import type { CAC } from "cac";
+import { getPackage } from "../utils";
 
-const filePath: string = path.join(__dirname, "../../epic-designer/dist/index.js");
+async function run() {
+  const pkg = await getPackage("epic-designer");
+  if (!pkg) return;
 
-if (fs.existsSync(filePath)) {
-  console.log("已满足启动条件，正在启动...");
-} else {
-  console.log("引用文件未编译，正在编译请稍等...");
-  exec(
-    "cd ../../ && npm run lib",
-    { maxBuffer: 50 * 1024 * 1024 },
-    (error: ExecException | null) => {
-      if (error) {
-        console.error(`执行 npm lib 时出错: ${error}`);
-        return;
+  // 定义目标文件的路径
+  const filePath: string = path.join(pkg.dir, "/dist/index.js");
+
+  // 检查目标文件是否存在
+  if (fs.existsSync(filePath)) {
+    // 如果文件存在，直接启动
+    console.log("已满足启动条件，正在启动...");
+  } else {
+    // 如果文件不存在，提示用户正在编译
+    console.log("引用文件未编译，正在编译请稍等...");
+
+    // 执行编译命令
+    exec(
+      "npm run build", // 切换到上级目录并执行 npm run lib 命令
+      { cwd: pkg.dir, maxBuffer: 50 * 1024 * 1024 }, // 设置最大缓冲区大小为50MB
+      (error: ExecException | null) => {
+        if (error) {
+          // 如果执行命令时出错，打印错误信息
+          console.error(`[checkCompiled]执行出错: ${error}`);
+          return;
+        }
+        // 编译成功，打印完成信息
+        console.log("编译完成");
       }
-      console.log(`编译完成`);
-    }
-  );
+    );
+  }
+}
+
+export function defineCheckCompiledCommand(cac: CAC) {
+  cac.command("checkCompiled").action(run);
 }
---
+++ scripts/src/index.ts
@@ -0,0 +1,9 @@
+import cac from 'cac';
+import { defineCheckCompiledCommand } from './check-compiled'
+// 创建 epic 实例
+const epic = cac('epic');
+
+defineCheckCompiledCommand(epic)
+
+// 解析命令行参数
+epic.parse();
