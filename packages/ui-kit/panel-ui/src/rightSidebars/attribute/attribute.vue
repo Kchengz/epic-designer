9daff5e3f5cbbf4ff389779e6238d7c54861111c
kchengz <1150791676@qq.com>
2025-12-31 10:49:52 +0800

    feat: ✨字段配置面板显示数据表名

--- packages/ui-kit/panel-ui/src/rightSidebars/attribute/attribute.vue
+++ packages/ui-kit/panel-ui/src/rightSidebars/attribute/attribute.vue
@@ -106,6 +106,17 @@ const componentAttributes = computed(() => {
       </div>
     </div>
     <!-- 组件id展示 end -->
+    <!-- 数据表 start -->
+    <div
+      v-if="dataTable?.tableRemark && designer.state.selectedNode?.input"
+      class="epic-attr-item mb-2 flex h-8 cursor-pointer items-center px-4"
+    >
+      <div class="epic-attr-label">数据表</div>
+      <div class="bg-$ep-secondary rounded-1 h-full flex-1 px-2 leading-8">
+        {{ dataTable.tableRemark }}
+      </div>
+    </div>
+    <!-- 数据表 end -->
     <div v-for="item in componentAttributes" :key="item.field">
       <EAttributeItem :schema="item" />
     </div>
