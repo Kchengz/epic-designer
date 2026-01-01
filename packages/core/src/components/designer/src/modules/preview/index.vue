4a858e404636196e2320ee4b1f1ee7eb7098c0b2
kcz <1150791676@qq.com>
2026-01-01 15:57:40 +0800

    fix: 🐛修复表单name不等于default时, 预览无法获取到表单数据问题

--- packages/core/src/components/designer/src/modules/preview/index.vue
+++ packages/core/src/components/designer/src/modules/preview/index.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
-import type { PageSchema } from '@epic-designer/types';
+import type { ComponentSchema, PageSchema } from '@epic-designer/types';
 
-import { inject, nextTick, ref } from 'vue';
+import { computed, inject, nextTick, ref } from 'vue';
 
 import { pluginManager } from '@epic-designer/manager';
-import { getUUID } from '@epic-designer/utils';
+import { findSchemas } from '@epic-designer/utils';
 
 import { EBuilder } from '../../../../builder';
 
@@ -27,7 +27,21 @@ const formValues = ref({});
 
 const pageSchema = inject('pageSchema') as PageSchema;
 const kb = ref<any>(null);
-const EBuilderKey = ref('');
+
+const getFormNames = computed(() => {
+  // 查找页面 schema 中所有类型为 'form' 的组件，并将其转换为 ComponentSchema[] 类型
+  const formNames = (
+    findSchemas(
+      pageSchema.schemas,
+      (schemas) => schemas.type.includes('form') && schemas.props?.name,
+    ) as ComponentSchema[]
+  )
+    // 提取每个 form 组件的名称（从 props 中获取 name 属性）
+    .map((schemas) => schemas.props?.name);
+
+  // 使用 Set 去重后返回表单组件名称数组
+  return [...new Set(formNames)];
+});
 
 function handleCloseData() {
   dataVisible.value = false;
@@ -39,37 +53,41 @@ function handleClose() {
 
 function handleOpen() {
   visible.value = true;
-  // 通过修改key重新渲染组件
-  EBuilderKey.value = getUUID();
 }
 
 async function handleOk() {
-  try {
-    // 遍历获取的表单组件名称并进行验证
-    let values = await kb.value.validateAll();
+  // 如果没有表单组件，则弹出提示并返回
+  if (!getFormNames.value.length) {
+    pluginManager.global.$message.error('缺少表单组件!');
+    return;
+  }
 
-    // 如果没有表单数据，则弹出提示并返回
-    if (Object.keys(values).length === 0) {
-      // eslint-disable-next-line no-alert
-      alert('请添加表单组件后再尝试！');
-      return;
+  try {
+    let values: any = {};
+
+    // 遍历获取的表单组件名称并进行验证
+    for (const name of getFormNames.value) {
+      values[`${name}`] = await kb.value.validate(name);
     }
 
-    // 如果只有一个表单组件，则将 values 赋值为表单数据的值
-    if (Object.keys(values).length === 1) {
+    // 如果只有一个表单组件且名称为 'default'，则将 values 赋值为 'default' 的值
+    if (
+      getFormNames.value.length === 1 &&
+      getFormNames.value[0] === 'default'
+    ) {
       values = values.default;
     }
 
     // 将验证后的表单数据转换为 JSON 字符串格式，并赋值给 formValues
     formValues.value = JSON.stringify(values, null, 2);
 
-    // 显示数据弹窗
-    dataVisible.value = true;
-
     // 在下一次 DOM 更新时，将 formValues 的值设置到 Monaco Editor 中
     nextTick(() => {
       monacoEditorRef.value?.setValue(formValues.value);
     });
+
+    // 显示数据弹窗
+    dataVisible.value = true;
   } catch (error) {
     // 捕获并输出错误
     console.error(error);
@@ -90,9 +108,8 @@ defineExpose({
     @close="handleClose"
     @ok="handleOk"
   >
-    <div class="min-w-750px rounded">
-      <EBuilder :key="EBuilderKey" ref="kb" :page-schema="pageSchema" />
-
+    <div class="min-w-750px translate-y-0px h-full rounded">
+      <EBuilder v-if="visible" ref="kb" :page-schema="pageSchema" />
       <!-- 表单数据 start -->
       <Modal
         v-model="dataVisible"
