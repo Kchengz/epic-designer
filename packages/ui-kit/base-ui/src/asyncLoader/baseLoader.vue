6206b270e434819fdd216685257deddd6c79b356
kcz <1150791676@qq.com>
2026-02-08 23:47:57 +0800

    chore: 调整基础loader样式

--- packages/ui-kit/base-ui/src/asyncLoader/baseLoader.vue
+++ packages/ui-kit/base-ui/src/asyncLoader/baseLoader.vue
@@ -2,7 +2,9 @@
 import { EpicIcon } from '../icon';
 </script>
 <template>
-  <div class="text-$ep-primary text-10 p-4 text-center">
+  <div
+    class="text-$ep-primary text-10 flex min-h-40 items-center justify-center p-4"
+  >
     <EpicIcon class="relative" name="icon--epic--bars-scale-fade" />
   </div>
 </template>
