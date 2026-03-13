ed2f7fb229b60d98046e9d573a974396fc241bb5
kcz <1150791676@qq.com>
2026-03-13 16:28:14 +0800

    feat: ✨新增isSubTable属性标识

--- packages/hooks/src/designer/useTableMeta.ts
+++ packages/hooks/src/designer/useTableMeta.ts
@@ -1,5 +1,6 @@
 import { computed } from 'vue';
 
+import { pluginManager } from '@epic-designer/manager';
 import { ComponentSchema, TableMeta } from '@epic-designer/types';
 
 import { useDesignerContext } from './useDesignerContext';
@@ -11,11 +12,14 @@ export function useTableMeta() {
       (item) => item.tableType === 'parent',
     );
 
-    // 倒序遍历 designer.state.matched，找到第一个 type = subform 数据
+    // 倒序遍历 designer.state.matched，找到第一个 isSubTable = true 数据
     const subTableNode = [...state.matched]
       .slice(0, -1) // 移除最后一个节点
       .reverse()
-      .find((node: ComponentSchema) => node.isSubTable);
+      .find((node: ComponentSchema) => {
+        const config = pluginManager.component.getConfigByType(node.type);
+        return config.isSubTable;
+      });
 
     // 根据是否存在子表单节点来查找对应的数据表
     tableMeta = subTableNode
