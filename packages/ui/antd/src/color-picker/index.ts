fc69a27747306313ff91a6ada38619f7f5e909e2
kcz <1150791676@qq.com>
2026-06-14 23:37:30 +0800

    release: 📦发布v1.1.13版本

--- packages/ui/antd/src/color-picker/index.ts
+++ packages/ui/antd/src/color-picker/index.ts
@@ -40,7 +40,6 @@ export default {
               value: 'small',
             },
           ],
-          
         },
         type: 'select',
       },
--- packages/ui/antd/src/radio/index.ts
+++ packages/ui/antd/src/radio/index.ts
@@ -39,7 +39,6 @@ export default {
               value: 'small',
             },
           ],
-          
         },
         type: 'select',
       },
